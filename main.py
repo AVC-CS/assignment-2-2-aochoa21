447cb085fc21840cd6f483ad2bdb159a16d041b3
aochoa21 <146290813+aochoa21@users.noreply.github.com>
2023-11-22 23:20:50 +0000

    Done Assignment 2-2

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def main():
     Use the same variables: celsius fahrenheit 
     ##################################################
     """
-
+    celsius = float(input("Enter the temperature in Celsius: "))
+    fahrenheit = (9 / 5) * celsius + 32
+    print(f"Fahrenheit: {fahrenheit:.2f}")
     """
     ########################################
     # Do not delete the return statement
